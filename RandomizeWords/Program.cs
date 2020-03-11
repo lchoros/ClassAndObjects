e74a3cd412f0cfd1949eb775113c62cc5845b286
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-03-11 16:46:55 +0000

    fractional

--- RandomizeWords/Program.cs
+++ RandomizeWords/Program.cs
@@ -10,6 +10,17 @@ namespace RandomizeWords
     {
         static void Main(string[] args)
         {
+            List<string> words = Console.ReadLine().Split(' ').ToList();
+            var randomizer = new Random();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                var position = randomizer.Next(0, words.Count);
+                var oldWord = words[i];
+                words[i] = words[position];
+                words[position] = oldWord;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
         }
     }
 }
