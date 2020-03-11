d08a22e45cbea974b607f81d17ec8871f5d01203
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-03-11 16:10:07 +0000

    first commit

---
+++ DayOfWeek1/DateService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek1
+{
+    public class DateService
+    {
+        public string GetDayOfWeekFromDate(string date)
+        {
+            var x = DateTime.ParseExact(date, "d-M-yyyy", CultureInfo.InvariantCulture);
+            return DateTime.ParseExact(date, "d-M-yyyy", CultureInfo.InvariantCulture).DayOfWeek.ToString();
+        }
+    }
+}
---
+++ DayOfWeek1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            var dateService = new DateService();
+            Console.WriteLine(dateService.GetDayOfWeekFromDate(date));
+        }
+    }
+}
