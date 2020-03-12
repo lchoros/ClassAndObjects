f08a53b7a1e09d22be730f180ff272d01416ab33
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-03-12 17:52:28 +0000

    m

---
+++ CountWorkingDays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountWorkingDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+            DateTime endDate = DateTime.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
