c6a6b92d30201251184563459c695f978602ca7c
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-03-18 17:03:02 +0000

    classes

---
+++ CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counter = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!counter.ContainsKey(numbers[i]))
+                {
+                    counter.Add(numbers[i], 0);
+                }
+                counter[numbers[i]]++;
+            }
+
+            foreach (var num in counter)
+            {
+                Console.WriteLine("{0} -> {1} times", num.Key, num.Value);
+            }
+        }
+    }
+}
---
+++ OddLines/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("input.txt").Where((x,i)=> i % 2 == 1).ToArray();
+            File.WriteAllLines("output.txt", data);
+        }
+    }
+}
